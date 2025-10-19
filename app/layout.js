68eefa57cb88fe29aed90a329683e7430a597940
Marwa Khot <marwakhot007@gmail.com>
2025-10-19 13:16:13 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,8 @@
-
 import './globals.css'
 
 export const metadata = {
   title: 'Chronicles: Rewritten',
   description: 'Walk the roads history forgotten.',
-  icons: {
-    icon: '/favicon.ico',
-  },
 }
 
 export default function RootLayout({ children }) {
