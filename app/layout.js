a95bbe9e86cd16f6c70f100298c433459979b198
Marwa Khot <marwakhot007@gmail.com>
2025-10-19 16:35:25 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import SessionProvider from '@/components/SessionProvider'
 
 export const metadata = {
   title: 'Chronicles: Rewritten',
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SessionProvider>
+          {children}
+        </SessionProvider>
+      </body>
     </html>
   )
 }
